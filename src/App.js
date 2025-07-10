2cd9d78c395571644033dbc604824330852fda81
Oleh Liubichenko <oleh.lubichenko@mirko.in.ua>
2025-07-11 01:49:11 +0300

    feature: add states for inputs in form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Beers", quantity: 2, packed: false },
   { id: 2, description: "Phones", quantity: 12, packed: false },
@@ -20,22 +22,29 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSumbit(e) {
     e.preventDefault();
-    console.log(e);
   }
 
   return (
     <form className="add-form" onSubmit={handleSumbit}>
       <h3>What do you need for the trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
