fe0786bee379450e66651c3391ca34db82d68379
thelegend2004 <oleh.lubichenko@mirko.in.ua>
2025-07-11 01:18:01 +0300

    feature: add item component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Beers", quantity: 2, packed: false },
+  { id: 2, description: "Phones", quantity: 12, packed: false },
+  { id: 3, description: "Pants", quantity: 3, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -10,7 +16,7 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>✈️Close Enough🛤️</h1>;
+  return <h1>✈️ Close Enough 🛤️</h1>;
 }
 
 function Form() {
@@ -22,12 +28,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
