4e5af6f24aa8192de460f557028c36af4baa0aa3
thelegend2004 <oleh.liubichenko@gmail.com>
2025-07-11 02:07:05 +0300

    feature: add reset to form

--- src/App.js
+++ src/App.js
@@ -27,12 +27,23 @@ function Form() {
 
   function handleSumbit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handleSumbit}>
       <h3>What do you need for the trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
